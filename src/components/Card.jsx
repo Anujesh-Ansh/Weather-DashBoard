c21f7a9a911aa02e9ef4fd24f58b56c381bae14f
Anujesh Ansh <anshanujesh@gmail.com>
2024-07-12 21:32:19 +0530

    Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import { Box, Container, Typography } from '@mui/material'
+import React from 'react'
+
+function Card() {
+  return (
+    <Container>
+        <Box>
+            <Typography>
+                Cards
+            </Typography>
+        </Box>
+    </Container>
+  )
+}
+
+export default Card
