eeb7e70f3be9b2cebb6c3e64acc95f751346a4e5
Anujesh Ansh <anshanujesh@gmail.com>
2024-07-12 21:31:18 +0530

    New Components

---
+++ src/components/SevenDayTemp.jsx
@@ -0,0 +1,16 @@
+import { Box, Container, Typography } from '@mui/material'
+import React from 'react'
+
+function SevenDayTemp() {
+  return (
+    <Container>
+        <Box>
+            <Typography>
+                7 Day Temperature
+            </Typography>
+        </Box>
+    </Container>
+  )
+}
+
+export default SevenDayTemp
